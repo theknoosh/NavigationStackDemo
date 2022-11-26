3dd285c7e0e1875443f014e8bff4e56e8abcb51c
Darrell Payne <darrellpayne@earthlink.net>
2022-11-26 15:52:40 -0800

    Initial Commit

---
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackDemoApp.swift
+//  NavigationStackDemo
+//
+//  Created by DARRELL A PAYNE on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
