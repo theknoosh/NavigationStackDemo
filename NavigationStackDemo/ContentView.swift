3b357b99f2bb112617b878ded69097c889fd7f7a
Darrell Payne <darrellpayne@earthlink.net>
2022-11-26 19:08:22 -0800

    Basic NavigationStack config with no customization

--- NavigationStackDemo/ContentView.swift
+++ NavigationStackDemo/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var platforms: [Platform] = [.init(name: "Xbox", imageName: "xbox.logo", color: .green),
+                                .init(name: "Playstation", imageName: "playstation.logo", color: .indigo),
+                                .init(name: "PC", imageName: "pc", color: .pink),
+                                .init(name: "Mobile", imageName: "iphone", color: .mint)]
+    
+    var games: [Game] = [.init(name: "Mincraft", rating: "99"),
+                         .init(name: "God of War", rating: "98"),
+                         .init(name: "Fortnite", rating: "92"),
+                         .init(name: "Madden 2023", rating: "88")]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Platforms") {
+                    ForEach(platforms, id: \.name) { platform in
+                        NavigationLink(value: platform){
+                            Label(platform.name, systemImage: platform.imageName)
+                                .foregroundColor(platform.color)
+                        }
+                    }
+                }
+                Section("Games") {
+                    ForEach(games, id: \.name) { game in
+                        NavigationLink(value: game){
+                            Text(game.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Gaming")
+            .navigationDestination(for: Platform.self) { platform in
+                ZStack {
+                    platform.color.ignoresSafeArea()
+                    Label(platform.name, systemImage: platform.imageName)
+                        .font(.largeTitle).bold()
+                }
+            }
+            .navigationDestination(for: Game.self) { game in
+                Text("\(game.name) - \(game.rating)")
+                    .font(.largeTitle).bold()
+            }
         }
-        .padding()
     }
 }
 
@@ -23,4 +59,15 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Platform: Hashable {
+    let name: String
+    let imageName: String
+    let color: Color
+}
+
+struct Game: Hashable {
+    let name: String
+    let rating: String
 }
